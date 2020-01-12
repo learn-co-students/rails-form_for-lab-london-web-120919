aeaa2d2cfca531e9f8d401515b0bf533d9f2c34d
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2020-01-12 18:15:32 +0000

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,38 @@
+class SchoolClassesController < ApplicationController
+    
+def index
+    @school_classes = SchoolClass.all
+end 
+
+def show 
+    @school_class = SchoolClass.find(params[:id])
+end 
+
+def new 
+    @school_class = SchoolClass.new
+    @submit_button = "Create School Class"
+end 
+
+def create
+    school_class = SchoolClass.create(post_params(:title, :number_room))
+
+    redirect_to school_class_path(school_class)
+end 
+
+def edit 
+    @school_class = SchoolClass.find(params[:id])
+end 
+
+def update 
+    school_class = SchoolClass.update(post_params(:title, :number_room))
+
+    redirect_to school_class_path(school_class)
+end 
+
+
+private 
+
+def post_params(*args)
+    params.require(:school_class).permit(*args)
+end 
+end  
